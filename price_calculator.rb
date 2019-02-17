f4fa42d1bbffccf66e6909afe323d856650d5198
David Mautz <dmautz@gmail.com>
2019-02-17 22:46:25 +0000

    Count the number of each item and create a checkout list

--- price_calculator.rb
+++ price_calculator.rb
@@ -1,3 +1,5 @@
+require 'pp'
+
 class GroceryCalculator
   
   ITEMS = {
@@ -19,11 +21,31 @@ class GroceryCalculator
     }
   }
   
-  def initialize(grocery_list)
-    
+  def initialize(list)
+    @total = 0
+    @saved = 0
+    @checkout_list = {}
+    count_items(list)
+    result
   end
   
   private
+  
+  def count_items(list)
+    list.split(',').map(&:strip).each do |item|
+      @checkout_list[item] = {quantity: 0, subtotal: 0} unless @checkout_list[item]
+      @checkout_list[item][:quantity] += 1
+    end
+  end
+  
+  def result
+    puts "Item     Quantity      Price"
+    puts "--------------------------------------"
+    pp @checkout_list
+    puts
+    puts "Total price : $#{@total}"
+    puts "You saved $#{@saved} today."
+  end
   
 end
 
