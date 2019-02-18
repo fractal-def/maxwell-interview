8966875ac4be853315c8c4373de0050d38cf5f2d
David Mautz <dmautz@gmail.com>
2019-02-18 00:31:03 +0000

    Subtotal items and print receipt

--- price_calculator.rb
+++ price_calculator.rb
@@ -1,17 +1,15 @@
-require 'pp'
-
 class GroceryCalculator
   
   ITEMS = {
     "Milk" => {
       price: 3.97,
-      salePrice: 5,
-      saleQuantity: 2
+      sale_price: 5,
+      sale_quantity: 2
     },
     "Bread" => {
       price: 2.17,
-      salePrice: 6,
-      saleQuantity: 3
+      sale_price: 6,
+      sale_quantity: 3
     },
     "Banana" => {
       price: 0.99
@@ -26,25 +24,42 @@ class GroceryCalculator
     @saved = 0
     @checkout_list = {}
     count_items(list)
-    result
+    subtotal_items
+    print_receipt
   end
   
   private
   
   def count_items(list)
-    list.split(',').map(&:strip).each do |item|
+    list.split(',').map(&:strip).map(&:capitalize).each do |item|
       @checkout_list[item] = {quantity: 0, subtotal: 0} unless @checkout_list[item]
       @checkout_list[item][:quantity] += 1
     end
   end
   
-  def result
+  def subtotal_items
+    @checkout_list.keys.each do |item| 
+      @checkout_list[item][:saved] = 0
+      full_price = @checkout_list[item][:quantity] * ITEMS[item][:price]
+      if ITEMS[item][:sale_quantity] && @checkout_list[item][:quantity] >= ITEMS[item][:sale_quantity] # if there is a sale and qty is sufficient
+          @checkout_list[item][:saved] = full_price - (ITEMS[item][:sale_price] + (@checkout_list[item][:quantity] % ITEMS[item][:sale_quantity] * ITEMS[item][:price])).round(2)
+      end
+      @checkout_list[item][:subtotal] = full_price - @checkout_list[item][:saved]
+      @total += @checkout_list[item][:subtotal]
+      @saved += @checkout_list[item][:saved]
+    end
+  end
+  
+  def print_receipt
+    puts
     puts "Item     Quantity      Price"
     puts "--------------------------------------"
-    pp @checkout_list
+    @checkout_list.each do |item,values|
+      puts "#{item}#{" "*(10-item.length)}#{values[:quantity]}#{" "*(13-values[:quantity].to_s.length)}$#{values[:subtotal]}"
+    end
     puts
     puts "Total price : $#{@total}"
-    puts "You saved $#{@saved} today."
+    puts "You saved $#{'%.2f' % @saved} today."
   end
   
 end
