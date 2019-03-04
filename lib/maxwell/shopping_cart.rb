b451dde2fa9ca6f0ed95c0220e178e917c65a22e
dave woodall <github@davewoodall.com>
2019-03-04 15:22:41 -0500

    Create Shopping Cart
    Create a ShoppingCart class which can:
    -  #add items
    -  view #items
    -  #total cart
    
    TODO:
    - [ ] Discount items
    - [ ] Write input script
    - [ ] Print output

--- lib/maxwell/shopping_cart.rb
+++ lib/maxwell/shopping_cart.rb
@@ -1,4 +1,50 @@
 module Maxwell
- class ShoppingCart
- end
+  class ShoppingCart
+
+    attr_reader :items
+
+    def initialize
+      @items = []
+    end
+
+    def add(item)
+      @items.push(item.name)
+      item
+    end
+
+    def total
+      sub_total_cart.reduce(0) { |sum, item| sum += item[2] }
+    end
+
+  private
+
+    def sub_total_cart
+      count_items.map do |item|
+        [ item[0], item[1], sub_total_item(item) ]
+      end
+    end
+
+    def group_items
+      @items.group_by { |i| i }
+    end
+
+    def get_price(item)
+      catalog = {
+        'milk' => 350,
+        'bread' => 200
+      }
+      price = catalog[item]
+      price.to_f / 100
+    end
+
+    def count_items
+      group_items.map do |item, quantity|
+        [item, quantity.length ]
+      end
+    end
+
+    def sub_total_item(item)
+      get_price(item[0]) * item[1]
+    end
+  end
 end
--- test/maxwell/shopping_cart_test.rb
+++ test/maxwell/shopping_cart_test.rb
@@ -2,8 +2,57 @@ require "test_helper"
 
 class Maxwell::ShoppingCartTest < Minitest::Test
   describe 'ShoppingCart' do
-    it 'start here' do
-      assert_equal true, true
+    let (:cart) { Maxwell::ShoppingCart.new }
+    let (:item) { Struct.new(:name) }
+    let (:milk) { item.new('milk') }
+    let (:bread) { item.new('bread') }
+
+    before do
+      3.times { cart.add(milk) }
+      2.times { cart.add(bread) }
+    end
+
+    describe 'attributes' do
+      it 'items' do assert cart.items end
+    end
+
+    describe 'Public methods' do
+      it '#total' do
+        expect = 14.50
+        assert_equal expect, cart.total
+      end
+
+      it '#add' do
+        assert_equal bread, cart.add(bread)
+      end
+    end
+
+    describe 'Private methods. OKAY to delete if they break' do
+      it '#sub_total_cart' do
+        expect = [['milk', 3, 10.50], ['bread', 2, 4.00]]
+        assert_equal expect, cart.send(:sub_total_cart)
+      end
+
+      it '#group_items' do
+        expect = {
+          "milk" => ["milk", "milk", "milk"],
+          "bread" => ["bread", "bread"]
+        }
+        assert_equal expect, cart.send(:group_items)
+      end
+
+      it '#group_items' do
+        expect = [
+          ['milk', 3],
+          ['bread', 2]
+        ]
+        assert_equal expect, cart.send(:count_items)
+      end
+
+      it '#get price' do
+        expect = 3.50
+        assert_equal expect, cart.send(:get_price, milk.name)
+      end
     end
   end
 end
