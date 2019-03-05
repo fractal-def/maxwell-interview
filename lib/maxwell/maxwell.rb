823578ff71ad4d0f3d16cb079931b67fca2bbdfb
dave woodall <github@davewoodall.com>
2019-03-05 12:21:00 -0500

    Create Printer Class
    A simple class which wraps cart interaction with presentation logic.
    No tests with this one.

--- lib/maxwell/maxwell.rb
+++ lib/maxwell/maxwell.rb
@@ -3,4 +3,5 @@ require "maxwell/version"
 module Maxwell
   class Error < StandardError; end
   require 'maxwell/shopping_cart'
+  require 'maxwell/printer'
 end
--- lib/maxwell/price_calculator.rb
+++ lib/maxwell/price_calculator.rb
@@ -1,39 +1,7 @@
-require_relative './shopping_cart'
+require_relative './printer.rb'
 
-Item = Struct.new(:name)
-cart = Maxwell::ShoppingCart.new
+printer = Maxwell::Printer.new
+printer.start
+printer.print_order
 
-puts 'Please enter all the items purchased separated by a comma'
 
-items = gets.chomp
-# puts "milk,milk, bread,banana,bread,bread,bread,milk,apple"
-# items = "milk,milk, bread,banana,bread,bread,bread,milk,apple"
-
-items.split(',').map { |i| i.strip}.map do |i|
-  item = Item.new(i)
-  cart.add(item)
-end
-
-def header
-  printf("%-12s %-15s %1s\n", "Item", "Quantity", "Price")
-  print "-" * 36
-  puts
-end
-
-def print_cart(cart)
-  cart.sub_total_cart.each do |item|
-    printf("%-19s %-8s $%s\n", item[0], item[1], item[2])
-  end
-  puts
-end
-
-if cart.total > 0
-  header
-  print_cart(cart)
-  puts "Your total is $#{cart.total}"
-  if cart.savings > 0
-    puts "You saved $#{cart.savings}"
-  end
-else
-  puts 'Sorry, we do not have anything of those things.'
-end
