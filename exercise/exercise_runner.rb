0b13c20ee180a9eea385304110f5c15952bef7fc
bnix <bnix@users.noreply.github.com>
2019-03-16 15:15:29 -0600

    Add simple output

--- exercise/exercise_runner.rb
+++ exercise/exercise_runner.rb
@@ -1,6 +1,8 @@
 class ExerciseRunner
   def self.run!(username:, config_path: nil)
     point_config = GithubEventConfig.new(config_path)
-    GithubEventScore.new(username, point_config).score
+    event_score = GithubEventScore.new(username, point_config)
+
+    puts "#{username}'s github score is #{event_score.score}"
   end
 end
