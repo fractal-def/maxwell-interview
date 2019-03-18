ece4509c7667e9b06d5003f7db7bcd162d2c2313
bnix <bnix@users.noreply.github.com>
2019-03-17 23:47:28 -0600

    Add #total_price, #total_saved to GroceryCalculator

--- price_calculator/grocery_calculator.rb
+++ price_calculator/grocery_calculator.rb
@@ -1,6 +1,4 @@
 class GroceryCalculator
-  attr_reader :groceries
-
   def initialize(groceries, config)
     grouped_groceries = groceries.each_with_object({}) do |name, memo|
       item = config.inventory[name]
@@ -9,5 +7,14 @@ class GroceryCalculator
     end
 
     @groceries = grouped_groceries.values
+  end
+
+  def total_price
+    @groceries.reduce(0.0) { |sum, group| sum + group.price }
+  end
+
+  def total_saved
+    presale_price = @groceries.reduce(0.0) { |sum, group| sum + group.presale_price }
+    presale_price - total_price
   end
 end
