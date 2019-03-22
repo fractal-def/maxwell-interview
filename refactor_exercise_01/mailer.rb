251cf2db0ff26fb682866aba3c4898be89d4c4c8
Auston Bunsen <auston.bunsen@gmail.com>
2019-03-22 18:39:55 -0400

    Updates to mailer for instance vars

--- refactor_exercise_01/mailer.rb
+++ refactor_exercise_01/mailer.rb
@@ -1,5 +1,4 @@
 class Emails < ActionMailer::Base
-
   def welcome(person)
     @person = person
     mail to: @person, from: 'foo@example.com'
@@ -11,18 +10,26 @@ class Emails < ActionMailer::Base
   end
   
   def admin_user_validated(admins, user)
-    @user = user
+    # we are using instance vars because the mailer view 
+    # uses them in its template
+    @user = user 
+    @admins = admins
     mail to: admins, from: 'foo@example.com'
   end
   
   def admin_new_user(admins, user)
+    # we are using instance vars because the mailer view 
+    # uses them in its template
     @user = user
+    @admins = admins
     mail to: admins, from: 'foo@example.com'
   end
 
   def admin_removing_unvalidated_users(admins, users)
+    # we are using instance vars because the mailer view 
+    # uses them in its template
     @users = users
+    @admins = admins
     mail to: admins, from: 'foo@example.com'
   end
-
 end
