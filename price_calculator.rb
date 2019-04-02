0799a5de26f07fccb2ca8d7025c79c6d8e2f3c0c
Kyle Lamy <flynfish@gmail.com>
2019-04-02 11:54:36 -0400

    Change to split on comma rather than regex

--- price_calculator.rb
+++ price_calculator.rb
@@ -1,12 +1,10 @@
 
 class PriceCalculator
 
-  INPUT_PATTERN = /\w+/
-
   def initialize
     puts "Please enter all the items purchased separated by a comma"
     user_input = gets.chomp
-    @items = user_input.scan(INPUT_PATTERN)
+    @items = user_input.split(',').map(&:strip)
 
   end
 
