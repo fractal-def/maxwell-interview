6be39504661c67eaa8eb073f3e762a02714819a7
Kyle Lamy <flynfish@gmail.com>
2019-04-02 11:36:25 -0400

    Initial commit, accept user input

---
+++ price_calculator.rb
@@ -0,0 +1,13 @@
+
+class PriceCalculator
+
+  def initialize
+    puts "Please enter all the items purchased separated by a comma"
+    user_input = gets.chomp
+
+  end
+
+
+end
+
+PriceCalculator.new
