34b423d7091297a32e434e1a1b10f27bfc9c1a9d
Kyle Lamy <flynfish@gmail.com>
2019-04-02 12:32:20 -0400

    Start of Item class and methods

--- price_calculator.rb
+++ price_calculator.rb
@@ -1,10 +1,44 @@
 
 class PriceCalculator
 
+  ITEMS = {
+    milk:   { unit_price: 3.97, sale_quantity: 2, sale_price: 5.00 },
+    bread:  { unit_price: 2.17, sale_quantity: 3, sale_price: 6.00 },
+    banana: { unit_price: 0.99 },
+    apple:  { unit_price: 0.89 }
+  }.freeze
+
   def initialize
     puts "Please enter all the items purchased separated by a comma"
     user_input = gets.chomp
     @items = user_input.split(',').map(&:strip)
+
+  end
+
+
+end
+
+class Item
+  require BigDecimal
+
+  def initalize(name:, unit_price:, quantity:, sale_quantity: nil, sale_price: nil )
+    @name = name
+    @unit_price = BigDecimal(unit_price) if unit_price.is_a? Numeric
+    @quantity = quantity
+    @sale_quantity = sale_quantity
+    @sale_price = BigDecimal(sale_price) if sale_price.is_a? Numeric
+  end
+
+  def total_price
+    @quantity * @unit_price
+  end
+
+  def total_savings
+    total_price - total_sale_price
+  end
+
+  def total_sale_price
+    return 0 unless @sale_price && @sale_quantity
 
   end
 
