be35c45e90d88f04c0fcf880dd36ad0ae7d2eaae
Kyle Lamy <flynfish@gmail.com>
2019-04-01 23:05:54 -0400

    Added scores, fetching commits and calculating scores

--- github_score.rb
+++ github_score.rb
@@ -9,16 +9,54 @@ require 'httparty'
 
 
 class GithubScore
+  # Any other type of commit gets a score of 1
+  COMMIT_SCORES = {
+    IssuesEvent: 7,
+    IssueCommentEvent: 6,
+    PushEvent: 5,
+    PullRequestReviewCommentEvent: 4,
+    WatchEvent: 3,
+    CreateEvent: 2
+  }.freeze
+
+  BASE_URL = "https://api.github.com/users/%s/events/public".freeze
 
   def initialize(username: 'dhh')
     @username = username
     @score = 0
+    @url = BASE_URL % @username
   end
 
   def result
     puts "#{@username.upcase}'s github score is #{@score}"
   end
+
+  def get_commits
+    response = HTTParty.get(
+      @url,
+      headers: {
+        "User-Agent" => "Httparty"
+      }
+    )
+
+    return false unless response.code == 200
+
+    return JSON.parse(response.body)
+  end
+
+  def score_commits
+    commits = get_commits
+
+    commits.each do |commit|
+      type = commit['type'].to_sym
+
+      next @score += 1 unless COMMIT_SCORES.has_key?(type)
+
+      @score += COMMIT_SCORES[type]
+    end
+  end
 end
 
 gh = GithubScore.new
+gh.score_commits
 gh.result
