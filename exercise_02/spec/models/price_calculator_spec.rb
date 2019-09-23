963da38b1c241043a460ac30a7d362d08a835135
Elli Park <elliparkdev@gmail.com>
2019-09-23 12:42:17 -0400

    Update test to reflect updated permission levels on PriceCalculator's methods

--- exercise_02/spec/models/price_calculator_spec.rb
+++ exercise_02/spec/models/price_calculator_spec.rb
@@ -4,21 +4,21 @@ RSpec.describe PriceCalculator do
   it 'correctly calculates the total and savings with items on sale' do
     items='milk,milk,bread,banana,bread,bread,bread,milk,apple'
     price_calculator = PriceCalculator.new(items)
-    expect(price_calculator.calculate_total).to eq(1902)
-    expect(price_calculator.calculate_savings).to eq(345)
+    expect(price_calculator.calculate_all_total).to eq(1902)
+    expect(price_calculator.calculate_all_savings).to eq(345)
   end
 
   it 'correctly calculates the total and savings without items on sale' do
     items='banana,banana,banana,apple,apple,apple, apple'
     price_calculator = PriceCalculator.new(items)
-    expect(price_calculator.calculate_total).to eq(653)
-    expect(price_calculator.calculate_savings).to eq(0)
+    expect(price_calculator.calculate_all_total).to eq(653)
+    expect(price_calculator.calculate_all_savings).to eq(0)
   end
 
   it 'correctly calculates the total and savings with items on sale but not meeting the minimum number of units' do
     items='milk,bread,bread,apple,apple,banana,banana'
     price_calculator = PriceCalculator.new(items)
-    expect(price_calculator.calculate_total).to eq(1207)
-    expect(price_calculator.calculate_savings).to eq(0)
+    expect(price_calculator.calculate_all_total).to eq(1207)
+    expect(price_calculator.calculate_all_savings).to eq(0)
   end
 end
