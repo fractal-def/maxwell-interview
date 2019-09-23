f3e544a4ee5e392034ef764f9d2b0150e1ef8001
Elli Park <elliparkdev@gmail.com>
2019-09-23 12:43:35 -0400

    Update to prompt for user input

--- exercise_02/price_calculator.rb
+++ exercise_02/price_calculator.rb
@@ -1,7 +1,6 @@
 require_relative 'models/price_calculator'
 
-# puts "Please enter all the items purchased separated by a comma:"
-# items = gets.chomp
-items = 'milk,milk,bread,banana,bread,bread,bread,milk,apple'
+puts "Please enter all the items purchased separated by a comma:"
+items = gets.chomp
 price_calculator = PriceCalculator.new(items)
 price_calculator.print_total
