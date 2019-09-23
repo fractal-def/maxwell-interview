ea7989b7635111739e77b96f65eba8ca7c3e0d05
Elli Park <elliparkdev@gmail.com>
2019-09-22 20:32:23 -0400

    Add generate_score method and snakecase helper method, call generate_score on dhh

--- exercise_01/git_hub_user.rb
+++ exercise_01/git_hub_user.rb
@@ -1,5 +1,4 @@
 require 'httparty'
-require 'pry'
 
 class GitHubUser
   SCORES = {
@@ -16,15 +15,38 @@ class GitHubUser
     @username = username
   end
 
+  def generate_score
+    fetch_commits
+    @score = 0
+    commit_types = @commits.map{|c| snakecase(c['type'])}
+
+    commit_types.each do |type|
+      @score += SCORES[type.to_sym] ? SCORES[type.to_sym] : SCORES[:other]
+    end
+
+    puts "#{@username}'s github score is #{@score}"
+  end
+
   private
 
   def api_endpoint
-    "https://api.github.com/users/#{@username}/events/public"
+    "https://api.github.com/users/#{@username.downcase}/events/public"
   end
 
   def fetch_commits
     @commits = HTTParty.get(api_endpoint)
   end
+
+  # From Rails' ActiveSupport file 'lib/core/facets/string/snakecase.rb', line 15
+  def snakecase(word)
+    word.gsub(/([A-Z]+)([A-Z][a-z])/,'\1_\2').
+    gsub(/([a-z\d])([A-Z])/,'\1_\2').
+    tr('-', '_').
+    gsub(/\s/, '_').
+    gsub(/__+/, '_').
+    downcase
+  end
 end
 
-dhh = GitHubUser.new('dhh')
+dhh = GitHubUser.new('DHH')
+dhh.generate_score
